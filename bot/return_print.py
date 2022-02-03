125af20baa8d8fdb2b6b5023bffbec628a33146c
xu-kai-xu <xuk1111@foxmail.com>
2022-02-03 10:04:28 +0800

    完成第一阶段练习

---
+++ bot/return_print.py
@@ -0,0 +1,9 @@
+q = 'How are you today?'
+def say(s):
+    return s
+    #print(s)
+
+def run(a):
+    return say(a)
+
+run(q)
---
+++ bot/return_use.py
@@ -0,0 +1,11 @@
+# q = 'How are you today?'
+# def say(s):
+#     return s
+
+# say(q)
+
+def a(b, c):
+    d = b + c
+    return d
+
+a(1, 2)
