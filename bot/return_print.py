c17281dd9d2ae47c2e849cd99c17ddc9838c8b87
xu-kai-xu <xuk1111@foxmail.com>
2022-09-09 16:05:22 +0800

    重建远程仓库

--- bot/return_print.py
+++
@@ -1,9 +0,0 @@
-q = 'How are you today?'
-def say(s):
-    return s
-    #print(s)
-
-def run(a):
-    return say(a)
-
-run(q)
--- vor/tree_node_test.py
+++
@@ -1,6 +0,0 @@
-from anytree import SymlinkNode, Node, RenderTree
-root = Node('root')
-s1 = Node("sub1", parent=root, bar=17)
-l = SymlinkNode(s1, parent=root, baz=18)
-l0 = Node("l0", parent=l)
-print(RenderTree(root))
